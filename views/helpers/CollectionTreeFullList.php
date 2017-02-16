bbb4d2c1bde87b751834cb5e7868f44d14459649
Julian Maurice <julian.maurice@biblibre.com>
2017-02-16 14:57:39 +0100

    Preload all collections when building the full collection tree
    This can save page load time up to 20% on large sets (tested with 2000+
    collections)

--- views/helpers/CollectionTreeFullList.php
+++ views/helpers/CollectionTreeFullList.php
@@ -11,6 +11,8 @@
  */
 class CollectionTree_View_Helper_CollectionTreeFullList extends Zend_View_Helper_Abstract
 {
+    protected $_collections;
+
     /**
      * Build a nested HTML unordered list of the full collection tree, starting
      * at root collections.
@@ -25,20 +27,53 @@ class CollectionTree_View_Helper_CollectionTreeFullList extends Zend_View_Helper
         if (!$rootCollections) {
             return null;
         }
+
         $collectionTable = get_db()->getTable('Collection');
-        $html = '<div id="collection-tree"><ul>';
-        foreach ($rootCollections as $rootCollection) {
+        $html = '<div id="collection-tree">';
+
+        $collectionTreeTable = get_db()->getTable('CollectionTree');
+        foreach ($rootCollections as &$rootCollection) {
+            $rootCollection['children'] = $collectionTreeTable->getDescendantTree($rootCollection['id']);
+        }
+
+        $html .= $this->_collectionTreeList($rootCollections, $linkToCollectionShow);
+        $html .= '</div>';
+
+        return $html;
+    }
+
+    protected function _collectionTreeList($collectionTree, $linkToCollectionShow)
+    {
+        $html = '<ul>';
+
+        foreach ($collectionTree as $collection) {
             $html .= '<li>';
             if ($linkToCollectionShow) {
-                $html .= link_to_collection(null, array(), 'show', $collectionTable->find($rootCollection['id']));
+                $html .= link_to_collection(null, array(), 'show', $this->_getCollection($collection['id']));
             } else {
-                $html .= $rootCollection['name'] ? $rootCollection['name'] : __('[Untitled]');
+                $html .= $collection['name'] ? $collection['name'] : __('[Untitled]');
             }
-            $collectionTree = get_db()->getTable('CollectionTree')->getDescendantTree($rootCollection['id']);
-            $html .= $this->view->collectionTreeList($collectionTree, $linkToCollectionShow);
+            $html .= $this->_collectionTreeList($collection['children'], $linkToCollectionShow);
             $html .= '</li>';
         }
-        $html .= '</ul></div>';
+
+        $html .= '</ul>';
+
         return $html;
+    }
+
+    protected function _getCollection($collectionId)
+    {
+        if (!isset($this->_collections)) {
+            $collectionTable = get_db()->getTable('Collection');
+            $this->_collections = array();
+            foreach ($collectionTable->findAll() as $collection) {
+                $this->_collections[$collection->id] = $collection;
+            }
+        }
+
+        if (isset($this->_collections[$collectionId])) {
+            return $this->_collections[$collectionId];
+        }
     }
 }
